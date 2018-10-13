353f74d875951d1129dd99d792486240a7f565f0
RostyslavAntonyshyn <rostyslav.antonyshyn@gmail.com>
2018-10-13 14:23:22 +0000

    Fixed exchanges and resolver start scripts

--- exchange-a/resolver/start.bash
+++ exchange-a/resolver/start.bash
@@ -1,19 +1,20 @@
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied"
+    echo "No arguments supplied, supported modes to swap are: btc_ltc, btc_xsn, ltc_xsn"
+    exit 1
 fi
 
-cd ~/go/src/github.com/ExchangeUnion/swap-resolver
+cd $GOPATH/go/src/github.com/ExchangeUnion/swap-resolver
 
 case "$1" in
   "btc_ltc")
-    go run resolver.go --listen localhost:7001 --peer localhost:7002 --lnd-rpc-ltc localhost:10001 --lnd-rpc-btc localhost:10002
+    ./resolver --listen localhost:7001 --peer localhost:7002 --lnd-rpc-ltc localhost:10001 --lnd-rpc-btc localhost:10002
     ;;
   "btc_xsn")
-    go run resolver.go --listen localhost:7002 --peer localhost:7003 --lnd-rpc-btc localhost:10002 --lnd-rpc-xsn localhost:10003
+    ./resolver --listen localhost:7002 --peer localhost:7003 --lnd-rpc-btc localhost:10002 --lnd-rpc-xsn localhost:10003
     ;;
   "ltc_xsn")
-    go run resolver.go --listen localhost:7001 --peer localhost:7003 --lnd-rpc-ltc localhost:10001 --lnd-rpc-xsn localhost:10003
+    ./resolver --listen localhost:7001 --peer localhost:7003 --lnd-rpc-ltc localhost:10001 --lnd-rpc-xsn localhost:10003
     ;;
   *)
     echo "Wrong argument passed, supported modes to swap are: btc_ltc, btc_xsn, ltc_xsn"
--- exchange-b/resolver/start.bash
+++ exchange-b/resolver/start.bash
@@ -1,19 +1,20 @@
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied"
+    echo "No arguments supplied, supported modes to swap are: btc_ltc, btc_xsn, ltc_xsn"
+    exit 1
 fi
 
-cd ~/go/src/github.com/ExchangeUnion/swap-resolver
+cd $GOPATH/src/github.com/ExchangeUnion/swap-resolver
 
 case "$1" in
   "btc_ltc")
-    go run resolver.go --listen localhost:7002 --peer localhost:7001 --lnd-rpc-ltc localhost:20001 --lnd-rpc-btc localhost:20002
+    ./resolver --listen localhost:7002 --peer localhost:7001 --lnd-rpc-ltc localhost:20001 --lnd-rpc-btc localhost:20002
     ;;
   "btc_xsn")
-    go run resolver.go --listen localhost:7003 --peer localhost:7002 --lnd-rpc-btc localhost:20002 --lnd-rpc-xsn localhost:20003
+    ./resolver --listen localhost:7003 --peer localhost:7002 --lnd-rpc-btc localhost:20002 --lnd-rpc-xsn localhost:20003
     ;;
   "ltc_xsn")
-    go run resolver.go --listen localhost:7003 --peer localhost:7001 --lnd-rpc-ltc localhost:20001 --lnd-rpc-xsn localhost:20003
+    ./resolver --listen localhost:7003 --peer localhost:7001 --lnd-rpc-ltc localhost:20001 --lnd-rpc-xsn localhost:20003
     ;;
   *)
     echo "Wrong argument passed, supported modes to swap are: btc_ltc, btc_xsn, ltc_xsn"
